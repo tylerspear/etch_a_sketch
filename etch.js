891d4c994db0d3c0f782b54e6b58d85d8d7c7ecd
Tyler Spear <stylerspear@gmail.com>
2019-07-19 23:30:28 -0500

    added mouseover functionality

--- etch.js
+++ etch.js
@@ -1,15 +1,27 @@
+//get html container as the root
 const container = document.querySelector("#container");
 
-//create 16 rows
+//create 16 rows and append to container
 for(let i = 0; i < 16; i++) {
     let row = document.createElement('div');
     row.className = "row";
     container.appendChild(row);
-
+    //create 16 colums and append to their row
     for(let j = 0; j < 16; j++) {
         let square = document.createElement('div');
         square.className = "square";
         row.appendChild(square);
     }
+}
+
+//get all squares
+let squares = document.querySelectorAll(".square");
+//loop over squares
+for(let i = 0; i < squares.length; i++) {
+    //add event listener for mouse over
+    squares[i].addEventListener('mouseover', function() {
+        //change background color
+        squares[i].classList.add('hovered');
+    })
 }
 
